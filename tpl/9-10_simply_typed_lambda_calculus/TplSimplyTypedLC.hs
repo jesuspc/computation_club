e7f0a6bf45489377d455bec2c2dd017e0bdd9ab2
Jesus Prieto Colomina <chus1818@gmail.com>
2017-05-21 17:50:49 +0100

    Minor

--- tpl/9-10_simply_typed_lambda_calculus/TplSimplyTypedLC.hs
+++ tpl/9-10_simply_typed_lambda_calculus/TplSimplyTypedLC.hs
@@ -15,18 +15,11 @@ data Term = TmVar Idx
 data Binding = Binding { bType :: Type }
 type Context = Map.Map Idx Binding
 
-addBind :: Idx -> Binding -> Context -> Context
-addBind = Map.insert
-
-getBind :: Idx -> Context -> Maybe Binding
-getBind = Map.lookup
-
-emptyCtx :: Context
-emptyCtx = Map.empty
-
 typeof :: Context -> Term -> Either String Type
-typeof ctx TmTrue = Right TypeBool
-typeof ctx TmFalse = Right TypeBool
+typeof _ TmTrue = Right TypeBool
+
+typeof _ TmFalse = Right TypeBool
+
 typeof ctx (TmIf cond t1 t2) = typeof ctx cond >>= \t' ->
   let branchType1 = typeof ctx t1
       branchType2 = typeof ctx t2
@@ -35,14 +28,26 @@ typeof ctx (TmIf cond t1 t2) = typeof ctx cond >>= \t' ->
   in case t' of
     TypeBool -> (==) <$> branchType1 <*> branchType2 >>= compare
     _        -> Left "Guard of conditional was not Bool"
+
 typeof ctx (TmVar i) =
   case getBind i ctx of
     Just binding -> Right (bType binding)
     Nothing      -> Left "Binding not found"
-typeof ctx (TmAbs i ty t) = typeof (addBind i (Binding ty) ctx) t >>= \ty2 -> Right $ TypeArrow ty ty2
+
+typeof ctx (TmAbs i ty t) = TypeArrow ty <$> typeof (addBind i (Binding ty) ctx) t
+
 typeof ctx (TmApp t1 t2) = (,) <$> typeof ctx t1 <*> typeof ctx t2 >>= compare
   where
     compare (TypeArrow t11 t12, t13)
       | t11 == t13 = Right t12
       | otherwise = Left "Invalid application: unmatched types"
     compare _ = Left "Invalid application"
+
+emptyCtx :: Context
+emptyCtx = Map.empty
+
+addBind :: Idx -> Binding -> Context -> Context
+addBind = Map.insert
+
+getBind :: Idx -> Context -> Maybe Binding
+getBind = Map.lookup
